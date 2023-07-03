e695245208b45e2c711d6e2dfefe66f16c8e902e
KumKeeHyun <kbzjung359@gmail.com>
2023-07-03 21:17:27 +0900

    controller: separate apis cluster and godis

--- pkg/controller/apis/godis/v1/register.go
+++ pkg/controller/apis/godis/v1/register.go
@@ -35,6 +35,8 @@ func Resource(resource string) schema.GroupResource {
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(
 		SchemeGroupVersion,
+		&GodisCluster{},
+		&GodisClusterList{},
 		&Godis{},
 		&GodisList{},
 	)
